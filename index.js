d4bb34a3dc5b382c768416d999331b3dafebefde
mahad <mahadaxmedguuleed22@gmail.com>
2023-09-07 10:21:58 -0700

    waan ka shaqeeyey assignmentigii

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // import your node server here
-import app from './api/server.js';
+import app from "./api/server.js";
 
 const port = 9000;
 
 // START YOUR SERVER HERE
 
+app.listen(port, () => console.log("listening on port 9000"));
